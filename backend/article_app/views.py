491a36b72b497cac0cabf3002688650206075b13
praivesi <praivesi@gmail.com>
2022-12-01 21:22:36 +0900

    feat: add serializers - TODO: add viewset for REST url

--- backend/article_app/views.py
+++ backend/article_app/views.py
@@ -1,5 +1,21 @@
+from lib2to3.pytree import Base
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Article
+from .serializers import BaseArticleSerializer
+
 
 def index(request):
     return HttpResponse('Hello world! This is article_app!')
+
+@api_view(['GET'])
+def ArticleAPI(request, id):
+    cur_article = Article()
+    cur_article.title = 'Article Title'
+    cur_article.author = 'Article Author'
+
+    serializer = BaseArticleSerializer(cur_article)
+
+    return Response(serializer.data)
