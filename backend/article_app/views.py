5458009b5064446d37b303646ab797a8410b5dab
praivesi <praivesi@gmail.com>
2022-12-03 19:34:35 +0900

    feat: add POST, PUT, DELETE API for Article

--- backend/article_app/views.py
+++ backend/article_app/views.py
@@ -1,11 +1,9 @@
 from lib2to3.pytree import Base
-from django.shortcuts import render
+from re import L, search
 from django.http import HttpResponse
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from rest_framework import viewsets
-from rest_framework import permissions
 from rest_framework.views import APIView
+from rest_framework import status
 from .models import Article
 from .serializers import BaseArticleSerializer
 
@@ -14,8 +12,48 @@ def index(request):
     return HttpResponse('Hello world! This is API Root!')
 
 class ArticleAPI(APIView):
-    def get(self, request):
-        queryset = Article.objects.all()
-        print(queryset)
-        serializer = BaseArticleSerializer(queryset, many = True)
-        return Response(serializer.data)
+    def post(self, request):
+        serializer = BaseArticleSerializer(data = request.data)
+
+        if not serializer.is_valid():
+            return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
+        serializer.save()
+        return Response(serializer.data, status = status.HTTP_201_CREATED)
+
+    def get(self, request, **kargs):
+        if kargs.get('article_id') is None:
+            queryset = Article.objects.all()
+            serializer = BaseArticleSerializer(queryset, many = True)
+        else:
+            article_id = kargs.get('article_id')
+            serializer = BaseArticleSerializer(Article.objects.get(id = article_id))
+
+        return Response(serializer.data, status = status.HTTP_200_OK)
+
+    def put(self, request, **kargs):
+        article_id = kargs.get('article_id')
+
+        if article_id is None:
+            return Response('Invalid request (Article ID is none)', status = status.HTTP_400_BAD_REQUEST)
+        
+        article = Article.objects.get(id = article_id)
+        serializer = BaseArticleSerializer(article, data = request.data)
+
+        if not serializer.is_valid():
+            return Response('Invalid request (Article could not be serialized)', status = status.HTTP_400_BAD_REQUEST)
+        
+        serializer.save()
+        return Response(serializer.data, status = status.HTTP_200_OK)
+            
+
+    def delete(self, request, **kargs):
+        article_id = kargs.get('article_id')
+
+        if article_id is None:
+            return Response('Invalid request (Article ID is none)', status = status.HTTP_400_BAD_REQUEST)
+        
+        article = Article.objects.get(id = article_id)
+        article.delete()
+
+        return Response(f'Article (id = {article_id}) deleted.', status = status.HTTP_200_OK)
