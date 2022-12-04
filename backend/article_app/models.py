5e5b89f5211ee25b7f8e75e3d730ebd76c231cf5
praivesi <praivesi@gmail.com>
2022-12-04 13:02:12 +0900

    feat: update Article model (add genre, content, create_date, update_date fields)

--- backend/article_app/models.py
+++ backend/article_app/models.py
@@ -2,10 +2,23 @@ from django.db import models
 
 # Create your models here.
 
+genre_choies = [
+    (0, 'None'),
+    (1, 'SwHw'),
+    (2, 'AdvancedCS'),
+    (3, 'Science'),
+    (4, 'Economics'),
+    (5, 'Philosophy'),
+    (6, 'Life')
+]
+
 class Article(models.Model):
     title = models.CharField(max_length=100)
+    genre = models.IntegerField(choices = genre_choies, default = 0)
     author = models.CharField(max_length=100)
-    date = models.DateTimeField(auto_now_add=True)
+    content = models.TextField(default = '')
+    create_date = models.DateTimeField(auto_now_add=True, blank = True)
+    update_date = models.DateTimeField(auto_now_add=True, blank = True)
 
     def __str__(self):
         return self.title
--- backend/article_app/serializers.py
+++ backend/article_app/serializers.py
@@ -4,5 +4,5 @@ from .models import Article
 class BaseArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
-        fields = ['title', 'author']
-        # fields = '__all__'
+        # fields = ['title', 'author']
+        fields = '__all__'
