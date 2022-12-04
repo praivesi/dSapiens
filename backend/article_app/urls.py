a6a079b301e642e47e0c1b8e676da191c2d89062
praivesi <praivesi@gmail.com>
2022-12-04 13:03:03 +0900

    feat: add POST, PUT, DELETE API for Article

--- backend/article_app/urls.py
+++ backend/article_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('articles/', views.ArticleAPI.as_view())
+    path('articles/', views.ArticleAPI.as_view()),
+    path('articles/<int:article_id>/', views.ArticleAPI.as_view())
 ]
