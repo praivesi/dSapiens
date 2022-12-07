9a5a7b48e63802aad4c688fc319468157d6a8af5
praivesi <praivesi@gmail.com>
2022-12-07 17:48:11 +0900

    feat: add default NavBar, need customizing

---
+++ frontend/dsapiens_frontend/src/routes/index.js
@@ -0,0 +1,49 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import Intro from "../components/views/Intro.vue";
+import Science from "../components/views/Science.vue";
+import Economics from "../components/views/Economics.vue";
+import Life from "../components/views/Life.vue";
+import SwHw from "../components/views/SwHw.vue";
+import AdvancedCS from "../components/views/AdvancedCS.vue";
+import Philosophy from "../components/views/Philosophy.vue";
+
+const routes = [
+  {
+    path: "/",
+    redirect: "/intro",
+  },
+  {
+    path: "/intro",
+    component: Intro,
+  },
+  {
+    path: "/swHw",
+    component: SwHw,
+  },
+  {
+    path: "/advancedCS",
+    component: AdvancedCS,
+  },
+  {
+    path: "/science",
+    component: Science,
+  },
+  {
+    path: "/economics",
+    component: Economics,
+  },
+  {
+    path: "/philosophy",
+    component: Philosophy,
+  },
+  {
+    path: "/life",
+    component: Life,
+  },
+];
+
+export default createRouter({
+  history: createWebHistory(),
+  routes,
+});
