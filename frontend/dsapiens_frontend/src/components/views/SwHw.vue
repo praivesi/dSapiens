1605799864287254e75fec4d6bd7b9c076615e30
praivesi <praivesi@gmail.com>
2022-12-10 03:08:34 +0900

    feat: bind editing, listing items in SwHw component

--- frontend/dsapiens_frontend/src/components/views/SwHw.vue
+++ frontend/dsapiens_frontend/src/components/views/SwHw.vue
@@ -1,6 +1,10 @@
 <template>
   <sw-hw-list v-if="state == 'list'" @edit="edit"></sw-hw-list>
-  <sw-hw-edit v-if="state == 'edit'"></sw-hw-edit>
+  <sw-hw-edit
+    v-if="state == 'edit'"
+    :title="item.title"
+    :content="item.content"
+  ></sw-hw-edit>
 </template>
 
 <script>
@@ -16,9 +20,11 @@ import SwHwList from "./SwHwList.vue";
 import SwHwEdit from "./SwHwEdit.vue";
 
 let state = ref("list");
+let item = ref(null);
 
-function edit(item) {
+function edit(edit_item) {
   state.value = "edit";
+  item.value = edit_item;
 }
 </script>
 
--- frontend/dsapiens_frontend/src/components/views/SwHwEdit.vue
+++ frontend/dsapiens_frontend/src/components/views/SwHwEdit.vue
@@ -1,10 +1,30 @@
 <template>
-  <QuillEditor theme="snow" :style="{ height: 70 + 'vh' }" />
+  <h1>{{ title }}</h1>
+  <div :style="{ height: 2 + 'vh' }"></div>
+  <QuillEditor ref="quill" theme="snow" :style="{ height: 70 + 'vh' }" />
 </template>
 
 <script setup>
-import { QuillEditor } from "@vueup/vue-quill";
+import { ref, onMounted, defineProps } from "vue";
+import { QuillEditor, Quill } from "@vueup/vue-quill";
 import "@vueup/vue-quill/dist/vue-quill.snow.css";
+
+let props = defineProps({
+  title: {
+    Type: String,
+    default: "Default Title",
+  },
+  content: {
+    Type: String,
+    default: "Default Content",
+  },
+});
+
+const quill = ref(null);
+
+onMounted(() => {
+  quill.value.setText(props.content);
+});
 </script>
 
 <style></style>
