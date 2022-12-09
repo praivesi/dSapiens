db67b55af24d6d5d12b585011a9a732f09a700b7
praivesi <praivesi@gmail.com>
2022-12-09 20:37:24 +0900

    feat: add vue-quill (Text Editor) &  add prettier plugin

--- frontend/dsapiens_frontend/src/components/views/SwHw.vue
+++ frontend/dsapiens_frontend/src/components/views/SwHw.vue
@@ -1,13 +1,18 @@
 <template>
-    Software, Hardware, Computer Science ...
+  <!-- Software, Hardware, Computer Science ... -->
+  <QuillEditor theme="snow" />
 </template>
 
 <script>
+import { QuillEditor } from "@vueup/vue-quill";
+import "@vueup/vue-quill/dist/vue-quill.snow.css";
+
 export default {
-    name: 'SwHwView'
-}
+  name: "SwHwView",
+  components: {
+    QuillEditor,
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
