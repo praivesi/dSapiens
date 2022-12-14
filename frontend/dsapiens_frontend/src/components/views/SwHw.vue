4bd72161edf23a515d32a14cb50185638bc7d953
praivesi <praivesi@gmail.com>
2022-12-15 01:00:04 +0900

    feat: add SwHwEdit & locate SwHwList, SwHwEdit in SwHw component

--- frontend/dsapiens_frontend/src/components/views/SwHw.vue
+++ frontend/dsapiens_frontend/src/components/views/SwHw.vue
@@ -1,23 +1,25 @@
 <template>
-  <!-- Software, Hardware, Computer Science ... -->
-  <!-- <QuillEditor theme="snow" :style="{ height: 70 + 'vh' }" /> -->
-  <sw-hw-list></sw-hw-list>
+  <sw-hw-list v-if="state == 'list'" @edit="edit"></sw-hw-list>
+  <sw-hw-edit v-if="state == 'edit'"></sw-hw-edit>
 </template>
 
 <script>
-// import { QuillEditor } from "@vueup/vue-quill";
-// import "@vueup/vue-quill/dist/vue-quill.snow.css";
-import SwHwList from "./SwHwList.vue";
-
 export default {
   name: "SwHwView",
-  components: {
-    SwHwList,
-  },
-  //   components: {
-  //     QuillEditor,
-  //   },
 };
+</script>
+
+<script setup>
+import { ref } from "vue";
+
+import SwHwList from "./SwHwList.vue";
+import SwHwEdit from "./SwHwEdit.vue";
+
+let state = ref("list");
+
+function edit(item) {
+  state.value = "edit";
+}
 </script>
 
 <style></style>
---
+++ frontend/dsapiens_frontend/src/components/views/SwHwEdit.vue
@@ -0,0 +1,10 @@
+<template>
+  <QuillEditor theme="snow" :style="{ height: 70 + 'vh' }" />
+</template>
+
+<script setup>
+import { QuillEditor } from "@vueup/vue-quill";
+import "@vueup/vue-quill/dist/vue-quill.snow.css";
+</script>
+
+<style></style>
