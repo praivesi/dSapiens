ad490e583265c6b0c99f37d3884fa9ee70e73a8c
praivesi <praivesi@gmail.com>
2022-12-19 19:03:17 +0900

    feat: add ScienceEdit, ScienceList vue components

---
+++ frontend/dsapiens_frontend/src/components/views/ScienceList.vue
@@ -0,0 +1,174 @@
+<template>
+  <v-card max-width="100%" class="mx-auto">
+    <v-list>
+      <template v-for="(item, index) in items">
+        <v-subheader
+          v-if="item.header"
+          :key="item.header"
+          v-text="item.header"
+        ></v-subheader>
+
+        <v-divider
+          v-else-if="item.divider"
+          :key="index"
+          :inset="item.inset"
+        ></v-divider>
+
+        <v-list-item
+          v-else
+          :key="item.title"
+          class="d-flex flex-column justfy-space-between"
+        >
+          <v-list-item-content
+            :style="{ width: 100 + '%', 'background-color': 'red' }"
+            class="d-flex justify-space-between"
+          >
+            <div :style="{ width: 100 + '%' }">
+              <v-list-item-title v-html="item.title"></v-list-item-title>
+              <v-list-item-subtitle
+                v-html="item.subtitle"
+              ></v-list-item-subtitle>
+            </div>
+            <!-- <div
+              :style="{ width: 100 + 'vh', 'background-color': 'blue' }"
+            ></div> -->
+          </v-list-item-content>
+          <v-btn @click="editClick(item)">Edit</v-btn>
+        </v-list-item>
+      </template>
+    </v-list>
+  </v-card>
+</template>
+
+<script setup>
+import { defineEmits } from "vue";
+
+let emit = defineEmits(["edit"]);
+
+function editClick(item) {
+  emit("edit", item);
+}
+</script>
+
+<script>
+export default {
+  name: "ScienceListView",
+  data: () => ({
+    items: [
+      {
+        avatar: "https://cdn.vuetifyjs.com/images/lists/1.jpg",
+        title: "Brunch this weekend?",
+        subtitle: `<span class="text--primary">Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?`,
+        content: "Bruch Content~~~~~~~~~~~~~~~~~~~~~~~!!",
+      },
+      { divider: true, inset: true },
+      {
+        avatar: "https://cdn.vuetifyjs.com/images/lists/2.jpg",
+        title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>',
+        subtitle: `<span class="text--primary">to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend.`,
+        content: "Bruch Content~~~~~~~~~~~~~~~~~~~~~~~!!",
+      },
+      { divider: true, inset: true },
+      {
+        avatar: "https://cdn.vuetifyjs.com/images/lists/3.jpg",
+        title: "Oui oui",
+        subtitle:
+          '<span class="text--primary">Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?',
+        content: "Bruch Content~~~~~~~~~~~~~~~~~~~~~~~!!",
+      },
+      { divider: true, inset: true },
+      {
+        avatar: "https://cdn.vuetifyjs.com/images/lists/4.jpg",
+        title: "Birthday gift",
+        subtitle:
+          '<span class="text--primary">Trevor Hansen</span> &mdash; Have any ideas about what we should get Heidi for her birthday?',
+        content: "Bruch Content~~~~~~~~~~~~~~~~~~~~~~~!!",
+      },
+      { divider: true, inset: true },
+      {
+        avatar: "https://cdn.vuetifyjs.com/images/lists/5.jpg",
+        title: "Recipe to try",
+        subtitle:
+          '<span class="text--primary">Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.',
+        content: "Bruch Content~~~~~~~~~~~~~~~~~~~~~~~!!",
+      },
+      {
+        avatar: "https://cdn.vuetifyjs.com/images/lists/1.jpg",
+        title: "Brunch this weekend?",
+        subtitle: `<span class="text--primary">Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?`,
+        content: "Bruch Content~~~~~~~~~~~~~~~~~~~~~~~!!",
+      },
+      { divider: true, inset: true },
+      {
+        avatar: "https://cdn.vuetifyjs.com/images/lists/2.jpg",
+        title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>',
+        subtitle: `<span class="text--primary">to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend.`,
+        content: "Bruch Content~~~~~~~~~~~~~~~~~~~~~~~!!",
+      },
+      { divider: true, inset: true },
+      {
+        avatar: "https://cdn.vuetifyjs.com/images/lists/3.jpg",
+        title: "Oui oui",
+        subtitle:
+          '<span class="text--primary">Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?',
+        content: "Bruch Content~~~~~~~~~~~~~~~~~~~~~~~!!",
+      },
+      { divider: true, inset: true },
+      {
+        avatar: "https://cdn.vuetifyjs.com/images/lists/4.jpg",
+        title: "Birthday gift",
+        subtitle:
+          '<span class="text--primary">Trevor Hansen</span> &mdash; Have any ideas about what we should get Heidi for her birthday?',
+        content: "Bruch Content~~~~~~~~~~~~~~~~~~~~~~~!!",
+      },
+      { divider: true, inset: true },
+      {
+        avatar: "https://cdn.vuetifyjs.com/images/lists/5.jpg",
+        title: "Recipe to try",
+        subtitle:
+          '<span class="text--primary">Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.',
+        content: "Bruch Content~~~~~~~~~~~~~~~~~~~~~~~!!",
+      },
+      {
+        avatar: "https://cdn.vuetifyjs.com/images/lists/1.jpg",
+        title: "Brunch this weekend?",
+        subtitle: `<span class="text--primary">Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?`,
+        content: "Bruch Content~~~~~~~~~~~~~~~~~~~~~~~!!",
+      },
+      { divider: true, inset: true },
+      {
+        avatar: "https://cdn.vuetifyjs.com/images/lists/2.jpg",
+        title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>',
+        subtitle: `<span class="text--primary">to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend.`,
+        content: "Bruch Content~~~~~~~~~~~~~~~~~~~~~~~!!",
+      },
+      { divider: true, inset: true },
+      {
+        avatar: "https://cdn.vuetifyjs.com/images/lists/3.jpg",
+        title: "Oui oui",
+        subtitle:
+          '<span class="text--primary">Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?',
+        content: "Bruch Content~~~~~~~~~~~~~~~~~~~~~~~!!",
+      },
+      { divider: true, inset: true },
+      {
+        avatar: "https://cdn.vuetifyjs.com/images/lists/4.jpg",
+        title: "Birthday gift",
+        subtitle:
+          '<span class="text--primary">Trevor Hansen</span> &mdash; Have any ideas about what we should get Heidi for her birthday?',
+        content: "Bruch Content~~~~~~~~~~~~~~~~~~~~~~~!!",
+      },
+      { divider: true, inset: true },
+      {
+        avatar: "https://cdn.vuetifyjs.com/images/lists/5.jpg",
+        title: "Recipe to try",
+        subtitle:
+          '<span class="text--primary">Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.',
+        content: "Bruch Content~~~~~~~~~~~~~~~~~~~~~~~!!",
+      },
+    ],
+  }),
+};
+</script>
+
+<style></style>
