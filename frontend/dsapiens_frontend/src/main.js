bdaf109b0a1e9ce5036029afec8170de48d79aad
praivesi <praivesi@gmail.com>
2022-12-23 01:00:06 +0900

    feat(FE): add redaxios plugin

--- frontend/dsapiens_frontend/src/main.js
+++ frontend/dsapiens_frontend/src/main.js
@@ -3,7 +3,10 @@ import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
 import { loadFonts } from "./plugins/webfontloader";
 import router from "./routes";
+import axios from "redaxios";
 
 loadFonts();
 
-createApp(App).use(vuetify).use(router).mount("#app");
+axios.defaults.withCredentials = true;
+
+createApp(App).use(vuetify).use(router).use(axios).mount("#app");
