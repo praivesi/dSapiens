cb0989312f62a30074415cf6d68fc51365143718
praivesi <praivesi@gmail.com>
2022-12-27 01:00:04 +0900

    WIP: feat: add REST API communicatino - ADD_ARTICLE

--- frontend/dsapiens_frontend/src/api/article.js
+++ frontend/dsapiens_frontend/src/api/article.js
@@ -5,20 +5,20 @@ export async function get() {
 }
 
 export async function add(title, content) {
-  return http.post("/articles", {
+  return http.post("/articles/", {
     title,
     content,
   });
 }
 
 export async function update(id, title, content) {
-  return http.put("/articles", {
+  return http.put("/articles/", {
     id,
     title,
     content,
   });
 }
 
-export async function dlete(id) {
+export async function remove(id) {
   return http.delete("/articles/" + id + "/");
 }
--- frontend/dsapiens_frontend/src/components/views/SwHwEdit.vue
+++ frontend/dsapiens_frontend/src/components/views/SwHwEdit.vue
@@ -1,21 +1,34 @@
 <template>
   <div class="d-flex justify-space-between">
-    <h1>{{ title }}</h1>
+    <h1>
+      <input v-model="targetTitle" />
+    </h1>
     <div>
-      <v-btn>Submit</v-btn>
+      <v-btn @click="submit()">Submit</v-btn>
       <v-btn class="ml-5" @click="$emit('close')">Back</v-btn>
     </div>
   </div>
   <div :style="{ height: 2 + 'vh' }"></div>
-  <QuillEditor ref="quill" theme="snow" :style="{ height: 70 + 'vh' }" />
+  <QuillEditor
+    ref="quill"
+    v-model="targetContent"
+    theme="snow"
+    :style="{ height: 70 + 'vh' }"
+  />
 </template>
 
 <script setup>
 import { ref, onMounted, defineProps } from "vue";
+import { useStore } from "vuex";
+
 import { QuillEditor, Quill } from "@vueup/vue-quill";
 import "@vueup/vue-quill/dist/vue-quill.snow.css";
 
 let props = defineProps({
+  id: {
+    Type: Number,
+    default: -1,
+  },
   title: {
     Type: String,
     default: "Default Title",
@@ -26,11 +39,39 @@ let props = defineProps({
   },
 });
 
+const store = useStore();
+
+let targetId = ref(-1);
+let targetTitle = ref("");
+let targetContent = ref("");
+
 const quill = ref(null);
 
 onMounted(() => {
-  quill.value.setText(props.content);
+  targetId.value = props.id;
+  targetTitle.value = props.title;
+  targetContent.value = props.content;
+  //   quill.value.setText(targetContent.value);
+  //   quill.value.setText(props.content);
 });
+
+async function submit() {
+  if (targetId.value == -1) {
+    // ADD
+    // store.dispatch("ADD_ARTICLE", targetTitle.value, quill.value.getText());
+
+    // store.dispatch("ADD_ARTICLE", targetTitle.value, targetContent.value);
+    store.dispatch("ADD_ARTICLE", "abc", "abcd");
+  } else {
+    // UPDATE
+    store.dispatch(
+      "UPDATE_ARTICLE",
+      targetId.value,
+      targetTitle.value,
+      quill.value.getText()
+    );
+  }
+}
 </script>
 
 <style></style>
