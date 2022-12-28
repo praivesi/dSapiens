09344727420f9fb8b30c4d995bb41ae9d7019602
praivesi <praivesi@gmail.com>
2022-12-29 01:00:05 +0900

    feat: add mdi icons

--- frontend/dsapiens_frontend/src/components/views/SwHwList.vue
+++ frontend/dsapiens_frontend/src/components/views/SwHwList.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="d-flex justify-space-between ma-5">
     <h1>Software / Hardward posts</h1>
-    <v-btn @click="$emit('add')">Add</v-btn>
+    <v-btn @click="$emit('add')"><v-icon>mdi-plus</v-icon></v-btn>
   </div>
   <v-card max-width="100%" class="mx-auto">
     <v-list-item-group v-model="items">
       <v-list-item v-for="(item, i) in items" :key="i">
-        <div class="d-flex">
+        <div
+          class="d-flex align-center pl-2 pr-2"
+          :style="{ height: 50 + 'px' }"
+        >
           <v-list-item-content :style="{ width: 100 + '%' }">
             <div :style="{ width: 100 + '%' }">
               <v-list-item-title v-html="item.title"></v-list-item-title>
@@ -16,9 +19,14 @@
             </div>
           </v-list-item-content>
           <div :style="{ width: 100 + '%' }"></div>
-          <v-btn @click="$emit('edit', item)">Edit</v-btn>
-          <v-btn class="ml-5" @click="remove(item)">Remove</v-btn>
+          <v-btn @click="$emit('edit', item)"
+            ><v-icon>mdi-playlist-edit</v-icon></v-btn
+          >
+          <v-btn class="ml-5" @click="remove(item)"
+            ><v-icon>mdi-close</v-icon></v-btn
+          >
         </div>
+        <v-divider></v-divider>
       </v-list-item>
     </v-list-item-group>
   </v-card>
