59eca666e597992b4d872658495930cc12d66a88
praivesi <praivesi@gmail.com>
2022-12-31 01:00:05 +0900

    feat: add ConfirmModal vue component

--- frontend/dsapiens_frontend/src/App.vue
+++ frontend/dsapiens_frontend/src/App.vue
@@ -1,17 +1,19 @@
 <template>
-    <div>
-        <banner />
-        <NavBar  />
-    </div>
+  <confirm-modal></confirm-modal>
+  <div>
+    <banner />
+    <NavBar />
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
-}
+  name: "App",
+};
 </script>
 
 <script setup>
-import Banner from './components/common/Banner.vue'
-import NavBar from './components/common/NavBar.vue'
+import Banner from "./components/common/Banner.vue";
+import NavBar from "./components/common/NavBar.vue";
+import ConfirmModal from "./components/modals/ConfirmModal.vue";
 </script>
--- frontend/dsapiens_frontend/src/components/modals/ConfirmModal.vue
+++ frontend/dsapiens_frontend/src/components/modals/ConfirmModal.vue
@@ -1,7 +1,42 @@
-<template></template>
+<template>
+  <v-dialog v-model="isOpen" width="500">
+    <v-card>
+      <v-card-title class="text-h5 grey lighten-2"> {{ title }} </v-card-title>
 
-<script>
-export default {};
+      <v-card-text> {{ description }} </v-card-text>
+      <div class="d-flex justify-end ma-3">
+        <v-btn @click="confirm()"><v-icon>mdi-check-bold</v-icon></v-btn>
+        <v-btn class="ml-5" @click="cancel()"><v-icon>mdi-close</v-icon></v-btn>
+      </div>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script setup>
+import { ref, computed } from "vue";
+import { useStore } from "vuex";
+
+const store = useStore();
+
+// let isOpen = ref(store.getters["confirmModal/IS_OPEN"]);
+let isOpen = computed(() => store.getters["confirmModal/IS_OPEN"]);
+let title = computed(() => store.getters["confirmModal/TITLE"]);
+let description = computed(() => store.getters["confirmModal/MESSAGE"]);
+
+// let title = ref(store.getters["confirmModal/TITLE"]);
+// let description = ref(store.getters["confirmModal/MESSAGE"]);
+
+function confirm() {
+  isOpen.value = false;
+
+  store.commit("confirmModal/CLOSE", true);
+}
+
+function cancel() {
+  isOpen.value = false;
+
+  store.commit("confirmModal/CLOSE", false);
+}
 </script>
 
 <style></style>
