6c5c8a2d169baefbdcde321271643f21a29595a3
praivesi <praivesi@gmail.com>
2023-01-15 23:05:01 +0900

    feat: add List, Post, Edit views into AdvancedCS view

--- frontend/dsapiens_frontend/src/components/views/AdvancedCS.vue
+++ frontend/dsapiens_frontend/src/components/views/AdvancedCS.vue
@@ -1,13 +1,67 @@
 <template>
-    Algorithm, AI, BigData ...
+  <list-view
+    v-if="state == 'list'"
+    title="Advanced Computer Science"
+    @add="add"
+    @edit="edit"
+    @post="post"
+  ></list-view>
+  <post-view
+    v-if="state == 'post'"
+    :item="item"
+    @edit="edit"
+    @list="list"
+  ></post-view>
+  <edit-view
+    v-if="state == 'edit'"
+    :item="item"
+    @post="post"
+    @list="list"
+  ></edit-view>
 </template>
 
 <script>
 export default {
-    name: 'AdvancedCSView'
+  name: "AdvancedCSView",
+};
+</script>
+
+<script setup>
+import { ref } from "vue";
+import { ArticleModel } from "@/components/models/article-model.js";
+
+import ListView from "./article/ListView.vue";
+import EditView from "./article/EditView.vue";
+import PostView from "./article/PostView.vue";
+
+let state = ref("list");
+let item = ref(null);
+
+function add() {
+  item.value = new ArticleModel();
+  itme.value.genre = 2;
+
+  state.value = "edit";
+}
+
+function edit(edit_item) {
+  item.value = edit_item;
+
+  state.value = "edit";
+}
+
+function post(post_item) {
+  item.value = post_item;
+
+  state.value = "post";
+}
+
+function list() {
+  state.value = "list";
+
+  item.value = new ArticleModel();
+  itme.value.genre = 2;
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
