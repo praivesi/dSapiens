12de84527a28d81a843b08875f70978aba9fe3d7
praivesi <praivesi@gmail.com>
2023-01-30 01:00:03 +0900

    feat: Add pagination features in Pagination Component

--- frontend/dsapiens_frontend/src/components/pagination/Pagination.vue
+++ frontend/dsapiens_frontend/src/components/pagination/Pagination.vue
@@ -8,7 +8,7 @@
     >
     <div v-for="page in pages" :key="page">
       <v-btn
-        :class="page == cur_page ? ' ds-grey' : ' ds-lightgrey'"
+        :class="page == cur_page + 1 ? ' ds-grey' : ' ds-lightgrey'"
         @click="pageClick(page)"
         >{{ page }}</v-btn
       >
@@ -29,13 +29,30 @@ export default { name: "PaginationComp" };
 <script setup>
 import { ref, onMounted } from "vue";
 
+const emits = defineEmits(["pageNum"]);
+
+const props = defineProps({
+  pageNum: {
+    type: Number,
+    default: 0,
+  },
+  minPage: {
+    type: Number,
+    default: 0,
+  },
+  maxPage: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const PAGE_CHUNK = 5;
+
 let pages = ref([]);
-let cur_page = ref(1);
+let cur_page = ref(props.pageNum);
 
 onMounted(() => {
-  for (var i = 0; i < 5; i++) {
-    pages.value.push(i + 1);
-  }
+  refreshPagelist(props.minPage);
 });
 
 function pageClick(page) {
@@ -43,26 +60,73 @@ function pageClick(page) {
 }
 
 function prevClick() {
-  if (cur_page.value == 1) {
+  if (cur_page.value == props.minPage) {
     return;
+  }
+
+  if (cur_page.value % PAGE_CHUNK == 0 && cur_page.value != props.minPage) {
+    refreshPagelist(cur_page.value - 1);
   }
 
   cur_page.value -= 1;
+
+  emits("pageNum", cur_page.value);
 }
 
 function nextClick() {
-  if (cur_page.value == 5) {
+  if (cur_page.value == props.maxPage) {
     return;
   }
+
+  if (cur_page.value % PAGE_CHUNK == 4 && cur_page.value != props.maxPage) {
+    refreshPagelist(cur_page.value + 1);
+  }
+
   cur_page.value += 1;
+
+  emits("pageNum", cur_page.value);
 }
 
 function frontClick() {
-  cur_page.value = 1;
+  if (cur_page.value < props.minPage + PAGE_CHUNK) {
+    cur_page.value = props.minPage;
+  } else {
+    cur_page.value -= PAGE_CHUNK;
+  }
+
+  refreshPagelist(cur_page.value);
+
+  emits("pageNum", cur_page.value);
 }
 
 function tailClick() {
-  cur_page.value = 5;
+  if (cur_page.value > props.maxPage - PAGE_CHUNK) {
+    cur_page.value = props.maxPage;
+  } else {
+    cur_page.value += PAGE_CHUNK;
+  }
+
+  refreshPagelist(cur_page.value);
+
+  emits("pageNum", cur_page.value);
+}
+
+function refreshPagelist(pivotPage) {
+  if (pivotPage < props.minPage || pivotPage > props.maxPage) {
+    return;
+  }
+
+  let startPage = parseInt(pivotPage / PAGE_CHUNK) * PAGE_CHUNK;
+
+  pages.value = [];
+
+  for (var i = startPage; i < startPage + PAGE_CHUNK; i++) {
+    if (i > props.maxPage) {
+      break;
+    }
+
+    pages.value.push(i + 1);
+  }
 }
 </script>
 
